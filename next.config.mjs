ab44d621a8035571b834895150e8b31461a99e9d
Yogeesh S <yogeesh.sryd@gmail.com>
2026-04-12 09:58:36 +0530

    chore: setup github pages deployment workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/hospital_landing',
+  assetPrefix: '/hospital_landing',
   trailingSlash: true,
   images: {
     unoptimized: true,
