474e74e8fd4bddfae08dee01c863b18de5461966
Yogeesh S <yogeesh.sryd@gmail.com>
2026-04-12 15:12:30 +0530

    chore: dynamically set basePath for local vs github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
+const isProd = process.env.NODE_ENV === 'production'
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let basePath = ''
+
+if (isGithubActions) {
+  // Replace with your repository name if it's different
+  basePath = '/hospital_landing'
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/hospital_landing',
-  // basePath: '/',
+  basePath: basePath,
   trailingSlash: true,
   images: {
     unoptimized: true,
